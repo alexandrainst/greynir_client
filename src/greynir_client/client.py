03df99b8ff833c94a4c0bc1d228d1b2008222040
Peter Bjørn Jørgensen <peter.jorgensen>
2024-01-02 11:04:18 +0100

    add translate_en_to_is function

--- src/greynir_client/client.py
+++ src/greynir_client/client.py
@@ -3,17 +3,40 @@
 import logging
 import requests
 
-from omegaconf import DictConfig
+from .datamodel import TranslationRequestData, LANGS, TranslationResponseData
 
 logger = logging.getLogger(__name__)
 
 
-def example_function(config: DictConfig) -> None:
-    """An example function for your project.
-
-    Args:
-        config: The Hydra config for your project.
+def translate_en_to_is(apikey: str, contents: list[str]) -> TranslationResponseData:
+    """Translate a list of strings from English to Icelandic.
     """
-    logger.info("Hello World!")
-    logger.info(f"Your config is: {config}")
+
+    header = {
+        "X-API-Key": apikey,
+        "Content-Type": "application/json",
+        "accept": "application/json",
+    }
+    contents = [
+            'Where is the museum? I want to buy bread and milk. I should buy a boat.',
+            'This fundamental plant growth research is also a key example of how Nasa is working to unlock agricultural innovations that could help us understand how plants might overcome stressful conditions in food-scarce areas here on Earth.',
+    ]
+    translation_request = TranslationRequestData(
+        contents=contents,
+        sourceLanguageCode=LANGS.en,
+        targetLanguageCode=LANGS.is_,
+        model="mbart-multi-doc",
+        domain=None,
+    )
+
+    response = requests.post(
+        url="https://api.greynir.is/translate/",
+        data=translation_request.model_dump_json(),
+        headers=header,
+    )
+    if response.status_code == 200:
+        translation_response = TranslationResponseData.model_validate_json(response.content)
+        return translation_response
+    else:
+        raise RuntimeError("Request returned status code %d, content: %s" % (response.status_code, response.text))
 
--- src/scripts/your_script.py
+++ src/scripts/your_script.py
@@ -8,7 +8,7 @@ Usage:
 import requests
 #from omegaconf import DictConfig
 
-from greynir_client.client import example_function
+from greynir_client.client import translate_en_to_is
 from greynir_client.datamodel import TranslationRequestData, LANGS, TranslationResponseData
 
 
@@ -22,33 +22,13 @@ def main() -> None:
     with open("apikey.txt", "r") as f:
         apikey = f.read().strip()
 
-    header = {
-        "X-API-Key": apikey,
-        "Content-Type": "application/json",
-        "accept": "application/json",
-    }
     contents = [
             'Where is the museum? I want to buy bread and milk. I should buy a boat.',
             'This fundamental plant growth research is also a key example of how Nasa is working to unlock agricultural innovations that could help us understand how plants might overcome stressful conditions in food-scarce areas here on Earth.',
     ]
-    translation_request = TranslationRequestData(
-        contents=contents,
-        sourceLanguageCode=LANGS.en,
-        targetLanguageCode=LANGS.is_,
-        model="mbart-multi-doc",
-        domain=None,
-    )
 
-    response = requests.post(
-        url="https://api.greynir.is/translate/",
-        data=translation_request.model_dump_json(),
-        headers=header,
-    )
-    if response.status_code == 200:
-        translation_response = TranslationResponseData.model_validate_json(response.content)
-        print(translation_response)
-    else:
-        raise RuntimeError("Request returned status code %d, content: %s" % (response.status_code, response.text))
+    translation_response = translate_en_to_is(apikey, contents)
+    print(translation_response)
 
 
 
